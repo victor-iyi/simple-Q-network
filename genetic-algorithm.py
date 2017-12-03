fba327657a32da133b4ac40b85fbb6d4343ce3d8
victor-iyiola <javafolabi@gmail.com>
2017-12-03 20:44:16 +0100

    Genetic Algorithm

---
+++ genetic-algorithm.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 03 December, 2017 @ 8:43 PM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
