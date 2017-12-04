53ee02ec6a0fe61bcde07460c02746f8c4ee980b
victor-iyiola <javafolabi@gmail.com>
2017-12-04 22:22:06 +0100

    Evolutionary algorithms

--- playground/genetic-algorithm.py
+++ playground/genetic-algorithm.py
@@ -67,17 +67,32 @@ if __name__ == '__main__':
     n_states = env.observation_space.n
     n_actions = env.action_space.n
     # Hyperparameters
-    n_policies = 200
-    n_generations = 10
-    n_fittest = 10
+    n_policies = 1000
+    n_generations = 30
+    n_fittest = 100
     # Loop thought the population
     start = time.time()
     policies = [gen_policies(n_states, n_actions) for _ in range(n_policies)]
     for gen in range(n_generations):
         # Evaluate the population
         scores = [eval_policy(env, policy) for policy in policies]
-        print(f'Generation {gen+1:,}\tScore = {max(scores):.2%}')
+        print(f'Generation {gen+1:,}\tScore = {max(scores):.2f}')
         # Select n best
         rank_idx = list(reversed(np.argsort(scores)))
-        fittest = [policies[idx] for idx in rank_idx]
+        fittest = [policies[idx] for idx in rank_idx[:n_fittest]]
         # Crossover
+        prob = np.array(scores) / np.sum(scores)
+        offsprings = [crossover(policies[np.random.choice(range(n_policies), p=prob)],
+                                policies[np.random.choice(range(n_policies), p=prob)])
+                      for _ in range(n_policies - n_fittest)]
+        # Mutation
+        mutant = [mutate(offspring, n_actions) for offspring in offsprings]
+        # Update population
+        policies = fittest
+        policies += mutant
+    end = time.time()
+    scores = [eval_policy(env, policy) for policy in policies]
+    best_score = max(scores)
+    best_policy = policies[scores.index(best_score)]
+    print(f'Running best policy with score = {best_score:.2f}\tTime taken = {end-start:.4f}')
+    run_episode(env, best_policy, render=True)
